fc28dfbda4d4b24610bf5b2ccb4f67fd2d6796fd
Miloš Manojlović <manojlovicsmilos@hotmail.com>
2018-08-12 01:03:22 +0200

    Changes

--- Slightly 2 Overbuilt/Assets/GridElementBehaviour.cs
+++ Slightly 2 Overbuilt/Assets/GridElementBehaviour.cs
@@ -11,15 +11,24 @@ public class GridElementBehaviour : MonoBehaviour
 	}
 	void Update ()
 	{
-		//gameObject.SetActive(Grid.Visible);
+		if(Grid.Visible && gameObject.transform.position.y < 0)
+		{
+			gameObject.transform.position = new Vector3(gameObject.transform.position.x, 0.025f, gameObject.transform.position.z);
+		}
+		else if(!Grid.Visible && gameObject.transform.position.y > 0)
+		{
+			gameObject.transform.position = new Vector3(gameObject.transform.position.x, - 0.03f, gameObject.transform.position.z);
+		}
 	}
 	void OnMouseOver()
     {
+		if(this._Element == null) return;
 		gameObject.GetComponent<Renderer>().material.color = new Color(1, 1, 1, 1);
         Grid.CursorLocation = this._Element.Location;
     }
     void OnMouseExit()
     {
+		if(this._Element == null) return;
 		gameObject.GetComponent<Renderer>().material.color = new Color(0.5f, 0.5f, 0.5f, 1);
         if(Grid.CursorLocation == this._Element.Location) Grid.CursorLocation = new Vector2(-1,-1);
     }
