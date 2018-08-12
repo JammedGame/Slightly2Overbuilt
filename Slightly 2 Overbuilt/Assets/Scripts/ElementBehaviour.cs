68a68d1289577ecb1884df8f943260067a05dd15
Unknown <pistacj@gmail.com>
2018-08-12 15:25:07 +0200

    Mesh

--- Slightly 2 Overbuilt/Assets/Scripts/ElementBehaviour.cs
+++ Slightly 2 Overbuilt/Assets/Scripts/ElementBehaviour.cs
@@ -51,17 +51,17 @@ public class ElementBehaviour : MonoBehaviour
 	{
 		for(int i = 0; i < this._Data.Fragments.Count; i++)
 		{
-			GameObject FragmentObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
-			FragmentObject.transform.localScale = new Vector3(Element.Size, Element.Size, Element.Size);
-			if (this._Data.Location.x < 0) FragmentObject.transform.position = new Vector3(0, - 1.2f * Element.Size, 0);
+			GameObject FragmentObject = GameObject.Instantiate((GameObject)Resources.Load("fragment"));
+            FragmentObject.transform.localScale = new Vector3(Element.Size*50, Element.Size*50, Element.Size*50);
+			if (this._Data.Location.x < 0) FragmentObject.transform.position = new Vector3(0, - 1.2f * Element.Size*50, 0);
 			else
 			{
-				float Vertical = 0.5f + this._Data.Floor * Element.Size;
+				float Vertical = 0.5f + this._Data.Floor * Element.Size*50;
 				Vector2 Location = new Vector2(this._Data.Location.x + this._Data.Layout.LocationDiff.x, this._Data.Location.y + this._Data.Layout.LocationDiff.y);
-				if(this._Data.Layout.Rotation == 0) FragmentObject.transform.position = new Vector3(((Location.x - 2) * Element.Size) + this._Data.Fragments[i].Offset.x, Vertical, - ((Location.y - 2) * Element.Size) - this._Data.Fragments[i].Offset.z);
-				else if(this._Data.Layout.Rotation == 1) FragmentObject.transform.position = new Vector3(((Location.x - 2) * Element.Size) + this._Data.Fragments[i].Offset.z, Vertical, - ((Location.y - 2) * Element.Size) + this._Data.Fragments[i].Offset.x);
-				else if(this._Data.Layout.Rotation == 2) FragmentObject.transform.position = new Vector3(((Location.x - 2) * Element.Size) - this._Data.Fragments[i].Offset.x, Vertical, - ((Location.y - 2) * Element.Size) + this._Data.Fragments[i].Offset.z);
-				else if(this._Data.Layout.Rotation == 3) FragmentObject.transform.position = new Vector3(((Location.x - 2) * Element.Size) - this._Data.Fragments[i].Offset.z, Vertical, - ((Location.y - 2) * Element.Size) - this._Data.Fragments[i].Offset.x);
+				if(this._Data.Layout.Rotation == 0) FragmentObject.transform.position = new Vector3(((Location.x - 2) * Element.Size*50) + this._Data.Fragments[i].Offset.x, Vertical, - ((Location.y - 2) * Element.Size*50) - this._Data.Fragments[i].Offset.z);
+				else if(this._Data.Layout.Rotation == 1) FragmentObject.transform.position = new Vector3(((Location.x - 2) * Element.Size*50) + this._Data.Fragments[i].Offset.z, Vertical, - ((Location.y - 2) * Element.Size*50) + this._Data.Fragments[i].Offset.x);
+				else if(this._Data.Layout.Rotation == 2) FragmentObject.transform.position = new Vector3(((Location.x - 2) * Element.Size*50) - this._Data.Fragments[i].Offset.x, Vertical, - ((Location.y - 2) * Element.Size*50) + this._Data.Fragments[i].Offset.z);
+				else if(this._Data.Layout.Rotation == 3) FragmentObject.transform.position = new Vector3(((Location.x - 2) * Element.Size*50) - this._Data.Fragments[i].Offset.z, Vertical, - ((Location.y - 2) * Element.Size*50) - this._Data.Fragments[i].Offset.x);
 			}
 			if(!this._Data.Construct) FragmentObject.GetComponent<Renderer>().material.color = this._Data.Paint;
 			else FragmentObject.GetComponent<Renderer>().material.color = new Color(1,1,1,1);
