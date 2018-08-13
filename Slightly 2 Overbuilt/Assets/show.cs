ee3520b402eafce4509e9bcc1e35b9e7ad42273f
Unknown <pistacj@gmail.com>
2018-08-13 18:05:58 +0200

    Changes

---
+++ Slightly 2 Overbuilt/Assets/show.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class show : MonoBehaviour {
+
+    static public GameObject resurs1;
+    static public GameObject resurs2;
+    static public GameObject resurs3;
+
+    void Start() {
+        resurs1 = GameObject.Find("resurs1");
+        resurs2 = GameObject.Find("resurs2");
+        resurs3 = GameObject.Find("resurs3");
+        resurs1.SetActive(false);
+        resurs2.SetActive(false);
+        resurs3.SetActive(false);
+
+    }
+
+    public void prikazi() {
+
+  
+        resurs1.SetActive(true);
+
+    }
+}
