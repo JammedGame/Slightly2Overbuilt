7e9da949d2ab852b651a8e840334116443dd035a
Unknown <pistacj@gmail.com>
2018-08-14 21:20:19 +0200

    menu/zvuk

--- Slightly 2 Overbuilt/Assets/Scenes/SceneToPlay.cs
+++ Slightly 2 Overbuilt/Assets/Scenes/SceneToPlay.cs
@@ -2,11 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+
 
 public class SceneToPlay : MonoBehaviour {
 
-    void playScene()
+
+    public void playScene()
     {
-        SceneManager.LoadScene(PlayScene);
+        SceneManager.LoadScene("PlayScene");
+    }
+
+    public void quitGame() {
+        Application.Quit();
+    }
+
+    public void soundONOFF() {
+
+        Debug.Log("radi dugme");
+        if (AudioListener.volume > 0f)
+            AudioListener.volume = 0f;
+        else
+            AudioListener.volume = 1f;
     }
 }
